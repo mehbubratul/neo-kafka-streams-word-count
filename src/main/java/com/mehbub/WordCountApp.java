d23619e7906137b3e51fa833134e3ef50dc061d9
mehbubratul <ratulewu@gmail.com>
2022-08-22 23:39:21 +0600

    first commit

---
+++ src/main/java/com/mehbub/WordCountApp.java
@@ -0,0 +1,50 @@
+package com.mehbub;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+
+/**
+ * Kafka Streams Word Count Application
+ * https://github.com/Programming-with-Mati/kafka-streams-word-count
+ * Steps:
+ * 1 > in docker :
+ */
+import java.util.*;
+
+public class WordCountApp {
+    public static void main(String[] args) {
+        System.out.println("Work count app");
+
+
+        // construct properties
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-app");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0");
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        // Build Topology
+        streamsBuilder.<String, String>stream("sentences")
+                .flatMapValues((readOnlyKey, value) -> Arrays.asList(value.toLowerCase().split(" ")))
+                .groupBy((key, value) -> value)
+                .count(Materialized.with(Serdes.String(), Serdes.Long()))
+                .toStream()
+                .to("word-count", Produced.with(Serdes.String(), Serdes.Long()));
+
+        KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties);
+
+        kafkaStreams.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+
+    }
+}
